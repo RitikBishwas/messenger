3d3c6128558ffd931ae3375e3553cab4e1d5ae4c
RitikBishwas <134728719+RitikBishwas@users.noreply.github.com>
2024-07-27 21:07:13 +0530

    second commit

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("", {
+			const socket = io("https://messengetalk.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
